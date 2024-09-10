3e5ad72d8b53b0a6e23a8ed6a72b876bba083d97
VAIBHAV KUMAWAT <97437528+vaibhavk10@users.noreply.github.com>
2024-09-10 22:37:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/admineatsphere/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.example.admineatsphere
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+
+        },3000)
+    }
+}
