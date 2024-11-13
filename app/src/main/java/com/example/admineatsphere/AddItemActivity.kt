168532c776b9b64665a328ebfc8e6270132fbee6
vaibhavk10 <vaibhavkumawat1003@gmail.com>
2024-11-13 21:51:18 +0530

    First Commit

--- app/src/main/java/com/example/admineatsphere/AddItemActivity.kt
+++ app/src/main/java/com/example/admineatsphere/AddItemActivity.kt
@@ -2,19 +2,33 @@ package com.example.admineatsphere
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.admineatsphere.databinding.ActivityAddItemBinding
 
 class AddItemActivity : AppCompatActivity() {
+    private val binding: ActivityAddItemBinding by lazy {
+        ActivityAddItemBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_add_item)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+        binding.selectedImage.setOnClickListener {
+            pickImage.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
         }
     }
+
+    private val pickImage: ActivityResultLauncher<PickVisualMediaRequest> =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            if (uri != null) {
+                binding.selectedImage.setImageURI(uri)
+            }
+        }
 }
+
--- app/src/main/java/com/example/admineatsphere/MainActivity.kt
+++ app/src/main/java/com/example/admineatsphere/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.example.admineatsphere
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.admineatsphere.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding : ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+        binding.addMenu.setOnClickListener{
+            val intent = Intent(this, AddItemActivity::class.java)
+            startActivity(intent)
         }
     }
 }
